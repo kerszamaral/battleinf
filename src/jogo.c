af066989e073d2dcf154ef0d56ac30386103a974
Ian Kersz <kerszamaral@gmail.com>
2022-03-19 06:19:19 -0300

    Added border and fixed score until 10000

--- src/jogo.c
+++ src/jogo.c
@@ -5,14 +5,15 @@
 #define SCREENHEIGHT 450
 #define TAMPLAYERX 348 //Tamanho original da imagem Escalar para player em 10%!!!!
 #define TAMPLAYERY 457 //Tamanho original da imagem Escalar para player em 10%!!!!
+#define BORDER 15 //borda do jogo
 
 int movey(int y) //Function for moving the player with WASD or Arrow keys
 {
     if (!IsKeyDown(KEY_A) && !IsKeyDown(KEY_D) && !IsKeyDown(KEY_LEFT) && !IsKeyDown(KEY_RIGHT)) //For Tank like controls
     {
-        if ( (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) && y >= (TAMPLAYERY/20.0)) //Escalar para o centro, portanto 20 e não 10
+        if ( (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) && y >= (TAMPLAYERY/20.0) + 45) //Escalar para o centro, portanto 20 e não 10
             y -= 5;
-        if ( (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) && y + (TAMPLAYERY/20.0) <SCREENHEIGHT) //Escalar para o centro, portanto 20 e não 10
+        if ( (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) && y + (TAMPLAYERY/20.0) < SCREENHEIGHT - BORDER + 6) //Escalar para o centro, portanto 20 e não 10 (fator de correção)
             y += 5;
     }
     return y;
@@ -22,9 +23,9 @@ int movex(int x)
 {
     if (!IsKeyDown(KEY_S) && !IsKeyDown(KEY_W)&& !IsKeyDown(KEY_DOWN) && !IsKeyDown(KEY_UP)) //For Tank like controls
     {
-        if ( (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && x >= (TAMPLAYERX/20.0)) //Escalar para o centro, portanto 20 e não 10
+        if ( (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && x >= (TAMPLAYERX/20.0) + BORDER) //Escalar para o centro, portanto 20 e não 10
             x -= 5;
-        if ( (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && x + (TAMPLAYERX/20.0) <SCREENWIDTH) //Escalar para o centro, portanto 20 e não 10
+        if ( (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && x + (TAMPLAYERX/20.0) < SCREENWIDTH - BORDER) //Escalar para o centro, portanto 20 e não 10
             x += 5;
     }
     return x;
@@ -52,11 +53,12 @@ int rot(int rot)
 int jogo(void)
 {
     int pscore = 0;
-    int pospx = 360, pospy = 100, rotp = 0; //Initial player position for testing
+    int pospx = 360, pospy = 100, rotp = 0; //Initial player position and rotation for testing
     int health = 3; //Player Health
+    int fase = 1;
 
-    Font fontTtf = LoadFontEx("resources/fonts/Roboto-Regular.ttf", 32, 0, 250); //Load da fonte roboto
     Texture2D tankplayer = LoadTexture("resources/images/player.png");//Load imagem do tanque do player
+    Rectangle screenSizerec = {0, 0, SCREENWIDTH, SCREENHEIGHT}; //Retangulo para a borda do jogo
     Rectangle sourcePlayer = { 0, 0, TAMPLAYERX, TAMPLAYERY}; //Retangulo do tamanho da image com posição 00
     Vector2 centroPlayer = {TAMPLAYERX / 20.0, TAMPLAYERY / 20.0}; //Vetor para achar o centro da imagem do player escalada a 10%
 
@@ -65,6 +67,10 @@ int jogo(void)
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
+        DrawRectangle(0, 0, SCREENWIDTH, 40, DARKGRAY);
+        DrawText(TextFormat("Fase %d", fase), SCREENWIDTH/2 - 6*10 , 0, 40, YELLOW);
+        DrawRectangleLinesEx(screenSizerec, 5, DARKGRAY);
+
         //Retangulo movel do player, precisa estar aqui pois caso contrario não conseguimos mover
         Rectangle destPlayer = {pospx, pospy, TAMPLAYERX / 10.0, TAMPLAYERY / 10.0 }; // tamanho da imagem escalado para 10%
 
@@ -72,8 +78,9 @@ int jogo(void)
         pospy = movey(pospy);
         rotp = rot(rotp);
 
-        DrawTextEx(fontTtf, TextFormat("Score: %i", pscore), (Vector2){SCREENWIDTH / 2 - 150, 40 }, (float)fontTtf.baseSize, 2, GRAY);
+        DrawText(TextFormat("Score: %i", pscore), SCREENWIDTH / 2 + 215, 5, 32, RED);
         DrawTexturePro(tankplayer, sourcePlayer, destPlayer, centroPlayer, rotp, WHITE);
+        
         if (IsKeyPressed(KEY_ENTER))
         {
             pscore += 1;
@@ -81,6 +88,5 @@ int jogo(void)
 
         EndDrawing();
     }
-    UnloadFont(fontTtf);
     return pscore;
 }
