75f5f3024c1fe356959b318c9877e4ce612d7a0c
Ian Kersz <kerszamaral@gmail.com>
2022-03-18 21:33:05 -0300

    Move commands tested

--- src/main.c
+++ src/main.c
@@ -1,26 +1,37 @@
 /* Ian Kersz - Cartão UFRGS: 00338368
-00.03.22
-
-in: 2 valores
-out: 1 valor 
+18.03.22
+BattleInf Jogo de tanquezinho lá do NES
 */
 
 #include "raylib.h"
+#include <stdio.h>
 
 int main(void)
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
+    int pospx = 360, pospy = 100;
     InitWindow(screenWidth, screenHeight, "Meu texto");
-
+    
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
+
         ClearBackground(RAYWHITE);
-        DrawText("Texto", 190, 200, 20, GRAY);
-        DrawRectangle(screenWidth/2 - 40, 100, 80, 80, RED);
+        if (IsKeyDown(KEY_W))
+            pospy += -5;
+        if (IsKeyDown(KEY_S))
+            pospy += 5;
+        if (IsKeyDown(KEY_A))
+            pospx += -5;
+        if (IsKeyDown(KEY_D))
+            pospx += 5;
+        
+        DrawText("Testing moving", 190, 200, 20, GRAY);
+        
+        DrawRectangle(pospx, pospy, 80, 80, RED);
 
         EndDrawing();
     }
