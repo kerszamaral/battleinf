7ef471e4de60a681998cc09bf2b1bfc7241aa8e6
Ian Kersz <kerszamaral@gmail.com>
2022-03-20 01:03:08 -0300

    Fixed game boundaries

--- src/jogo.c
+++ src/jogo.c
@@ -1,21 +1,21 @@
 #include "raylib.h"
 #include "jogo.h"
-#include <stdio.h>
 
-#define SCREENWIDTH 800
-#define SCREENHEIGHT 450
-#define BORDER 15 //borda do jogo
+#define SCREENWIDTH 800 //Screen size x
+#define SCREENHEIGHT 450 //Screen size y
+#define TOPBORDER 50 //Menu border
+#define BORDER 5 //Game border
 
-typedef struct
+typedef struct //struct for the interactable objects in the game
 {
-    Vector2 pos;
-    Vector2 cen;
-    int health;
-    int rot;
-    int score;
-    int time;
-    float speed;
-    bool ammo;
+    Vector2 pos; //Vector2 for position, has x and y
+    Vector2 cen; //Vector2 for center position, has x and y
+    int health; //Int for health, for player
+    int rot; //Int for object rotation
+    int score; //int for game score, for player 
+    int time; //Int for time, for bullets and enemys
+    float speed; //int for speed, defines the speed objects move
+    bool ammo; //bool for defining if the object can shoot or is alive
 } Obj;
 
 int jogo(void)
@@ -25,8 +25,8 @@ int jogo(void)
     Obj player = {(Vector2){ 0 , 0 }, (Vector2){ 348/20.0 , 457/20.0 }, 3 , 0 , 0 , 0 , 5 , true };
     //                                   cen        x          y           rot     time     ammo
     //Random starting position player.cenx
-    player.pos.x = GetRandomValue( BORDER + player.cen.x , SCREENWIDTH - BORDER - player.cen.x ); //To start player in random position inbounds
-    player.pos.y = GetRandomValue( 40 + BORDER + player.cen.y , SCREENHEIGHT - BORDER - player.cen.y ); //To start player in random position inbounds
+    player.pos.x = GetRandomValue( BORDER*2 + player.cen.x , SCREENWIDTH - BORDER*2 - player.cen.x ); //To start player in random position inbounds
+    player.pos.y = GetRandomValue( 40 + BORDER*2 + player.cen.y , SCREENHEIGHT - BORDER*2 - player.cen.y ); //To start player in random position inbounds
     //Textures
     Texture2D tankplayer = LoadTexture( "resources/images/player.png" );//Load imagem do tanque do player
     //Rectangles for collision and drawing (dest rectangles need to be in while loop)
@@ -46,8 +46,8 @@ int jogo(void)
     //Textures
     Texture2D healthimg = LoadTexture( "resources/images/health.png" ); //Load imagem da vida do player
     //Rectangles for collision and drawing (dest rectangles need to be in while loop)
-    Rectangle playspaceSizerec = { 0 , 0 , SCREENWIDTH - BORDER , SCREENHEIGHT - BORDER }; //Retangulo para a borda do jog
-    Rectangle topMenurec = { 0 , 0 , SCREENWIDTH , 40 }; //Retangle for the ingame manu
+    Rectangle playspaceSizerec = { 0 , 0 , SCREENWIDTH , SCREENHEIGHT }; //Retangulo para a borda do jog
+    Rectangle topMenurec = { 0 , 0 , SCREENWIDTH , TOPBORDER }; //Retangle for the ingame manu
 
     //Main game loop
     while( !WindowShouldClose() && player.health != 0 ) //End if you press esc or player.health gets to 0
@@ -59,7 +59,7 @@ int jogo(void)
         /********************** MENU CREATION *******************************/
         //Draws solid color rectangles
         DrawRectangleRec( topMenurec , DARKGRAY ); //Creates the big grey bar for text
-        DrawRectangleLinesEx( playspaceSizerec , -5 , DARKGRAY ); //Creates the border for the playspace
+        DrawRectangleLinesEx( playspaceSizerec , 5 , DARKGRAY ); //Creates the border for the playspace
         //Text
         DrawText( TextFormat( "Fase %d" , level ) , SCREENWIDTH/2 - 6*10 , 0 , 40 , YELLOW );
         DrawText( TextFormat( "Score: %i", player.score ), SCREENWIDTH / 2 + 215 , 5 , 32 , RED );
@@ -76,26 +76,26 @@ int jogo(void)
         /********************** PLAYER MOVEMENT *******************************/
         if (!IsKeyDown(KEY_A) && !IsKeyDown(KEY_D) && !IsKeyDown(KEY_LEFT) && !IsKeyDown(KEY_RIGHT)) 
         {   //For Tank like controls
-            if ( (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) && !CheckCollisionRecs(destPlayer, topMenurec) ) 
-            {                                    //Checks if player is not colliding with top of plyaspace
+            if ( (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) && player.pos.y > player.cen.y + TOPBORDER + 2) 
+            {                                   //Checks if player center is not colliding with top of playspace + length + correction factor
                 player.pos.y -= player.speed;
                 player.rot = 0; //Sets players rotation to up
             }
-            if ( (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) && player.pos.y + player.cen.y < SCREENHEIGHT - BORDER + 6) //Escalar para o centro, portanto 20 e não 10 (fator de correção)
-            {
+            if ( (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) && player.pos.y + player.cen.y < SCREENHEIGHT - BORDER - 2)
+            {                                   //Checks if player center + length is not colliding with bottom of playspace and border + correction factor
                 player.pos.y += player.speed;
                 player.rot = 180; //Sets player rotation to down
             }
         }
         if (!IsKeyDown(KEY_S) && !IsKeyDown(KEY_W)&& !IsKeyDown(KEY_DOWN) && !IsKeyDown(KEY_UP)) //For Tank like controls
         {
-            if ( (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && player.pos.x >= player.cen.x + BORDER) //Escalar para o centro, portanto 20 e não 10
-            {
+            if ( (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && player.pos.x > player.cen.x + BORDER*2.5 +2 )
+            {                                   //Checks if player center is not colliding with left of playspace + lenght + correction factor
                 player.pos.x -= player.speed;
                 player.rot = 270; //Sets player rotation to left
             }
-            if ( (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && player.pos.x + player.cen.x < SCREENWIDTH - BORDER) //Escalar para o centro, portanto 20 e não 10
-            {
+            if ( (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) && player.pos.x + player.cen.x < SCREENWIDTH - BORDER*2.5 - 2)
+            {                                   //Checks if player center + lenght is not colliding with right of playspace and border + correction factor
                 player.pos.x += player.speed;
                 player.rot = 90; //Sets player rotation to right
             }
